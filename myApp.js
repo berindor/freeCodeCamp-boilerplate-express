18b269e71f905a9a6f7b8b57148d202c8bd4a804
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-26 16:09:32 +0100

    added hello word

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
 let express = require('express');
 let app = express();
 
+console.log('Hello Word');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
