dad009664703b351b838b54094af8adeccce1cd4
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-30 15:40:13 +0100

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 const absolutePathPublic = __dirname + '/public';
 app.use('/public', express.static(absolutePathPublic));
@@ -11,7 +12,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/json', function (req, res) {
-  res.json({ message: 'Hello json' });
+  res.json({ message: process.env.MESSAGE_STYLE === 'uppercase' ? 'HELLO JSON' : 'Hello json' });
 });
 
 module.exports = app;
